6c2a13da306c4b977708f1f2902893d582164a18
tbturk <tbturkk@gmail.com>
2019-12-17 15:13:37 +0300

    Fonksiyonlar düzeltildi
    ekle fonksiyonu değiştirildi, plaka bul fonksiyonu eklendi., plakaları tek yönlü düğüme atama hala eksik. manuel ekle foksiyonu adapte edilebilir.

--- main.c
+++ main.c
@@ -18,14 +18,16 @@ struct dugum2 { //tek boyutlu int arraylist olacak.
     struct dugum2* onceki;
     struct dugum2* sonraki;
 };
-struct dugum1* ilk=NULL,*son=NULL;
+struct dugum1 *ilk = NULL,*son = NULL, *son_eklenen;
 
-void ekle(int plaka,char *sehir,char *bolge, int komsu_sayisi);
+void ekle(int plaka);
 void sil(int x) ;
 void listele();
 void dosyadan_ekle();
-
+int plaka_bul(char *sehir_ismi,int cursor);
 FILE *dosya; // sehirler.txt ye her yerden ulasabilmek icin global tanımladik.
+int sehir_sayisi=1;// satir sayisi enter sayisinin bir fazlası olacagi icin 1 den baslattik.
+
 
 int main() {
     dosya = fopen("sehirler.txt","r");//sehirlerin bulundugu dosya okuma modunda acılıyor.
@@ -35,14 +37,14 @@ int main() {
         printf("\t\t\t\tsehir listesini iceren dosya basarili bir sekilde acildi...\n");
     }
     dosyadan_ekle();
-    ekle(25,"Eskisehir","AN",5);
+    /*ekle(25,"Eskisehir","AN",5);
     ekle(23,"Erzgkykyk","DA",3);
     ekle(27,"Erzurum","DA",3);
     ekle(29,"Erzurum","DA",3);
     ekle(28,"Duzce","AN",5);
     ekle(24,"Ankara","AN",6);
     ekle(29,"Istanbul","MA",1);
-    ekle(26,"Istanbul","MA",1);
+    ekle(26,"Istanbul","MA",1);*/
     printf("\n");
     listele();
     sil(23);
@@ -62,56 +64,91 @@ int main() {
 void dosyadan_ekle() {
 
     char sehir_satiri[100];
-    int sehir_sayisi=1;// satir sayisi enter sayisinin bir fazlası olacagi icin 1 den baslattik.
     int ch;
     for(ch = fgetc(dosya); ch!=EOF; ch = getc(dosya)) {
         if(ch == '\n') {
             sehir_sayisi++;
         }
     }
-
+    int cursor;
     fseek(dosya,0,SEEK_SET); // cursoru basa almazsak dosyayı okuyamayiz cunku sehir sayisini bulurken cursorun yeri degisti.
 
     for(int i=0; i<sehir_sayisi; i++) { // elde edilen satirlari(sehirleri) virgule gore parcaliyoruz.
         int plaka;
+        int komsu_plaka;
         char * sehir;
         char * bolge;
         int komsu_sayisi;
         fgets(sehir_satiri,100,dosya);
         strtok(sehir_satiri,"\n");
-        printf("\n%s\n",sehir_satiri);
-        const char *ptr = strtok(sehir_satiri,",");
+        // printf("\n%s\n",sehir_satiri);
+        char *ptr = strtok(sehir_satiri,",");
         int ayrac=0;// her bir satirda cursorun hangi virgulde kaldigini tutmamiz icin.
         komsu_sayisi=0; // komsu sayisini her dongude sifirlamamiz lazim.
         while(ptr != NULL) {
-            printf("'%s'\n", ptr);
+            struct dugum2 *last=NULL;
+            cursor=ftell(dosya);
+            if(ayrac==0) {
+                plaka=atoi(ptr);
+                ekle(plaka);
+            } else if(ayrac==1) {
+                const size_t len_my_str = strlen(ptr) + 1;
+                sehir = malloc(len_my_str);
+                strncpy(sehir, ptr, len_my_str);
+                son_eklenen->sehir=sehir;
+            } else if(ayrac==2) {
+                const size_t len_my_str = strlen(ptr) + 1;
+                bolge = malloc(len_my_str);
+                strncpy(bolge, ptr, len_my_str);
+                son_eklenen->bolge=bolge;
+            } else { //bu kisma komsularin plakasinin da atanmasi gelecek;
+                komsu_sayisi++;
+                komsu_plaka=plaka_bul(ptr,cursor);
+
+                //bu kisimda son_eklenen dugumun sub sehirlerine atama yapilacak.
+            }
+            ptr = strtok(NULL, ",");
+            ayrac++;
+        }
+        son_eklenen->komsu_sayisi=komsu_sayisi;
+    }
+}
+
+int plaka_bul(char *sehir_ismi,int cursor) {
+    fseek(dosya,0,SEEK_SET);
+    for(int i=0; i<sehir_sayisi; i++) { // elde edilen satirlari(sehirleri) virgule gore parcaliyoruz.
+        int plaka;
+        char * sehir;
+        char sehir_satiri[100];
+        fgets(sehir_satiri,100,dosya);
+        strtok(sehir_satiri,"\n");
+        const char *ptr = strtok(sehir_satiri,",");
+        int ayrac=0;// her bir satirda cursorun hangi virgulde kaldigini tutmamiz icin.
+        while(ptr != NULL) {
             if(ayrac==0) {
                 plaka=atoi(ptr);
             } else if(ayrac==1) {
                 const size_t len_my_str = strlen(ptr) + 1;
                 sehir = malloc(len_my_str);
                 strncpy(sehir, ptr, len_my_str);
-            } else if(ayrac==2) {
-                const size_t len_my_str = strlen(ptr) + 1;
-                bolge = malloc(len_my_str);
-                strncpy(bolge, ptr, len_my_str);
-            } else {//bu kisma komsularin plakasinin da atanmasi gelecek;
-                komsu_sayisi++;
+                if(strcmp(sehir_ismi,sehir)==0) {
+                    fseek(dosya,cursor,SEEK_SET);
+                    return plaka;
+                }
             }
             ptr = strtok(NULL, ",");
             ayrac++;
         }
-        ekle(plaka,sehir,bolge,komsu_sayisi);
-        //printf("\n%d %s %s komsu sayisi:%d    \n",plaka,sehir,bolge,komsu_sayisi);
     }
+    //bu kisima plaka bulunmaz ise eklemek ister misiniz secenegi eklenecek.
+    fseek(dosya,cursor,SEEK_SET);
+    return 0;
 }
 
-void ekle(int plaka,char *sehir,char *bolge, int komsu_sayisi) {
+void ekle(int plaka) {
     struct dugum1 *yeni = (struct dugum1*) malloc(sizeof(struct dugum1));
     struct dugum1 *eklenecek_yer=ilk;
     yeni->plaka=plaka;
-    yeni->sehir=sehir;
-    yeni->bolge=bolge;
     if(ilk==NULL) {
         ilk=yeni;
         son=yeni;
@@ -160,7 +197,9 @@ void ekle(int plaka,char *sehir,char *bolge, int komsu_sayisi) {
         }
 
     }
+    son_eklenen=yeni;
 }
+
 
 void sil(int x) {
     struct dugum1 *sil=ilk,*prev_1 = NULL;
@@ -203,5 +242,5 @@ void listele() {
     }
     printf("%d\t",liste->plaka);
     printf("%s\t",liste->bolge);
-    printf("%s\t",liste->sehir);
+    printf("%s\t\n" ,liste->sehir);
 }
